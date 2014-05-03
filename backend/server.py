e60aa0cb5d1cb9b94764fce264c985673b0c5c8d
Christian Panton <christian@panton.org>
2014-05-03 11:13:14 +0200

    moving /graph endpoint to seperate module

--- backend/server.py
+++ backend/server.py
@@ -3,10 +3,6 @@
 from flask import Flask, request, redirect, url_for
 from db import *
 
-import json
-
-# TEMP IMPORTS
-from random import random
 
 app = Flask(__name__)
 
@@ -15,57 +11,10 @@ app = Flask(__name__)
 def index():
     return redirect(url_for('static', filename="index.html"))
 
-@app.route('/graph/nodes/<int:id>', methods=['GET'])
-@app.route('/graph/nodes/', methods=['GET'])
-@app.route('/graph/nodes/<string:identifier>', methods=['GET'])
-def get_nodes(id=None, identifier=None):
 
-    if id:
-        node = db().find(Node, Node.id == id)
-        export = node.one().serialize()
-    elif identifier:
-        node = db().find(Node, Node.identifier == identifier)
-        export = node.one().serialize()
-    else:
-        all_nodes = db().find(Node)
-        export = map(lambda node: node.serialize(), all_nodes)
-   
-    return json.dumps(export)
-
-
-@app.route('/graph/edges/', methods=['GET'], defaults={"id": None})
-@app.route('/graph/edges/<int:id>', methods=['GET'])
-def get_edges(id):
-
-    if id:
-        edge = db().find(Edge, Edge.id == id)
-        export = edge.one().serialize()
-    else:
-        all_edges = db().find(Edge)
-        export = map(lambda edge: edge.serialize(), all_edges)
-   
-    return json.dumps(export)
-
-
-
-@app.route('/graph/node')
-def get_node():
-
-    export = {}
-
-    all_nodes = db().find(Node)
-    all_edges = db().find(Edge)
-
-    export["nodes"] = map(lambda node: node.serialize(), all_nodes)
-    export["edges"] = map(lambda edge: edge.serialize(), all_edges)
-
-    return json.dumps(export)
-
-
-@app.route('/graph/add', methods=["POST"])
-def graph_add():
-    print "Got", request.form
-    return 'Hello, JC'
+# /graph
+import endpoint_graph
+app.register_blueprint(endpoint_graph.mod)
 
 if __name__ == '__main__':
     app.run(debug=True)
